eaf5f69ab95cf92d51ac4e150e54867785d96b52
Justin Green <justingreendev@gmail.com>
2023-04-12 11:47:56 -0500

    Update comments template again

--- comments.php
+++ comments.php
@@ -25,6 +25,8 @@
    * Place this snippet below where ever you want to render the comments
    * Then remove this snippet from this file because it is intended to be used elsewhere, NOT from within this template   
    * Uncomment the snippet below when ready to use
+   * More info about the comment template: 
+   * https://developer.wordpress.org/themes/template-files-section/partial-and-miscellaneous-template-files/comment-template/
   **/
   
   // If comments are open or we have at least one comment, load up the comment template.
