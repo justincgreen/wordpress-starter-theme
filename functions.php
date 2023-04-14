574aeccc0b4480e899c3b945861a797df942e48e
Justin Green <justingreendev@gmail.com>
2023-04-14 11:13:08 -0500

    Add menu option in WP Dashboard

--- functions.php
+++ functions.php
@@ -5,4 +5,7 @@
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  */
 
+
+// Enable menu option to appear in WP dashboard - apperance/menus
+ add_theme_support( 'menus' );
 ?>
