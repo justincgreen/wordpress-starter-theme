b00c2ed856fd607ffe680c5eaa5757dd9173827f
Justin Green <justingreendev@gmail.com>
2023-04-13 21:29:26 -0500

    Updated header template

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php the_title(); ?></title>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
